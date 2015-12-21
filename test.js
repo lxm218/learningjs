c8fda31855eadc58eb34f5b16296ac353af06a64
Lan <lxm218@gmail.com>
2015-12-20 19:58:12 -0800

    update

---
+++ test.js
@@ -0,0 +1,7 @@
+var ohnoesSays = document.getElementById("ohnoes-says");
+
+var winstonSays = document.getElementById("winston-says");
+
+ohnoesSays.innerHTML="because I miss my buddy <em>Winston</em>";
+
+winstonSays.textContent="I miss him too !!";
---
+++ test2.js
@@ -0,0 +1,4 @@
+var avatarEls = document.getElementsByTagName("img");
+for (var i=0; i <avatarEls.length; i++){
+    avatarEls[i].height="50";
+}
